e6176b2d1bf02038d18f1bda95d09774dec2c260
ershad <ershad@gmail.com>
2017-11-06 21:43:42 +0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatDialogModule, MatSnackBarModule, MatTableModule, MatTooltipModule} from '@angular/material';
+import {DialogDemoComponent} from './dialog-demo/dialog-demo.component';
+import {MyDialogComponent} from './my-dialog/my-dialog.component';
+import { TooltipDemoComponent } from './tooltip-demo/tooltip-demo.component';
+import { SnackbarDemoComponent } from './snackbar-demo/snackbar-demo.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UserService} from './services/user.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogDemoComponent,
+    MyDialogComponent,
+    TooltipDemoComponent,
+    SnackbarDemoComponent,
+    UsertableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
+  entryComponents: [MyDialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
